8009e37aae508929e87979bc11231b2fa3fb65cf
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-12-08 19:57:31 -0300

    feat: criado pasta para __tests__ e criado suite de teste na home

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import Home from "@/app/page";
+
+beforeEach(() => render(<Home />));
+
+describe("Home", () => {
+  it("render Home component without errors", () => {
+    expect(screen.queryAllByTestId("sobre")).toBeTruthy();
+  });
+
+  it("render correct title on section 'sobre'", () => {
+    const heading = screen.getByRole("heading", {
+      name: "Designer Karen Rodrigues",
+    });
+    expect(heading).toBeDefined();
+  });
+});
