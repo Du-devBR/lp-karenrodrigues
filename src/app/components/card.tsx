02a72c31541838b2e6e403962fb747300f79365e
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-12-11 18:52:13 -0300

    feat: criado estilo para os cards

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -6,12 +6,15 @@ interface CardProps {
 }
 
 export default function Card({ data }: CardProps) {
+  console.log(data);
   return (
-    <div>
+    <div className="max-w-[276px] bg-brand-200 rounded-sm">
       <Image src={data.image} alt="" />
-      <div>
-        <h3>{data.name}</h3>
-        <p>{data.description}</p>
+      <div className="py-6 px-4 flex flex-col gap-3">
+        <h3 className="text-20 text-gray-900 font-bold max-md:text-16">
+          {data.name}
+        </h3>
+        <p className="text-14 text-gray-600 font-light">{data.description}</p>
       </div>
     </div>
   );
