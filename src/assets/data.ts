549938002503f868e9cd63123904cf4a740ff872
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-12-11 18:49:57 -0300

    feat: alterado estrutura do objeto para atender os servicos

--- src/assets/data.ts
+++ src/assets/data.ts
@@ -1,29 +1,67 @@
-import { ServicosItem } from "@/app/page";
+
 import imagemLinha from "@/../public/imagem-linha.png";
 
-export const arrayServicosSobrancelhas: ServicosItem[] = [
-  {
-    image: imagemLinha,
-    name: "Limpeza basica",
-    description:
-      "Type and scrambled it to make a type specimen book. It has survived not only five centuries.",
-  },
-  {
-    image: imagemLinha,
-    name: "Limpeza basica",
-    description:
-      "Type and scrambled it to make a type specimen book. It has survived not only five centuries.",
-  },
-  {
-    image: imagemLinha,
-    name: "Limpeza basica",
-    description:
-      "Type and scrambled it to make a type specimen book. It has survived not only five centuries.",
-  },
-  {
-    image: imagemLinha,
-    name: "Limpeza basica",
-    description:
-      "Type and scrambled it to make a type specimen book. It has survived not only five centuries.",
-  },
-];
+
+export const arrayServicos = {
+  servicos: [
+    {
+        title: "sobrancelhas premium",
+        card: [
+          {
+            image: imagemLinha,
+            name: "Limpeza basica",
+            description:
+              "Type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+          },
+          {
+            image: imagemLinha,
+            name: "Limpeza basica",
+            description:
+              "Type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+          },
+          {
+            image: imagemLinha,
+            name: "Limpeza basica",
+            description:
+              "Type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+          },
+          {
+            image: imagemLinha,
+            name: "Limpeza basica",
+            description:
+              "Type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+          },
+        ]
+
+    },
+    {
+        title: "hydragloss premium",
+        card: [
+          {
+            image: imagemLinha,
+            name: "Limpeza basica",
+            description:
+              "Type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+          },
+          {
+            image: imagemLinha,
+            name: "Limpeza basica",
+            description:
+              "Type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+          },
+          {
+            image: imagemLinha,
+            name: "Limpeza basica",
+            description:
+              "Type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+          },
+          {
+            image: imagemLinha,
+            name: "Limpeza basica",
+            description:
+              "Type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+          },
+        ]
+      }
+  ]
+}
