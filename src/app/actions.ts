cf4c6a0b830efff96122ef2636ce85f0d2c6215c
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-12-11 18:50:44 -0300

    feat: adicionado revalidate

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,7 +1,8 @@
+
 export async function getData() {
-  const res = await fetch("http://localhost:3000/api/servicos");
+  const revalidarDadosACada24Horas = 86400
+  const res = await fetch("http://localhost:3000/api/servicos", {next: {revalidate: revalidarDadosACada24Horas}});
 
   const data = await res.json()
-
   return data;
 }
