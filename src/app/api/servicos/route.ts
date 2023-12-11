bea33ee2a3134b4b80d9f327e8424a6d673041a4
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-12-11 19:11:22 -0300

    docs: documentado routes servicos

--- src/app/api/servicos/route.ts
+++ src/app/api/servicos/route.ts
@@ -1,9 +1,37 @@
-import { arrayServicosSobrancelhas } from "@/assets/data"
+import { arrayServicos } from "@/assets/data"
+
+/**
+   * Esta função chama o objeto arrayServicos, do arquivo data.ts.
+   * Futuramente deve-se alterar esse trecho de codigo por uma chamada fetch a uma api externa
+   *
+   * @async
+   * @function
+   * @returns {Response.JSON()}
+   * @throws {Error}
+  */
 
 export async function GET(){
-  const res = {
-    json: () => Promise.resolve(arrayServicosSobrancelhas)
-  };
-  const data = await res.json();
-  return Response.json(data)
+
+
+  try {
+  // Variável para simular a resposta de uma API externa
+    const res = {
+      json: () => Promise.resolve(arrayServicos)
+    };
+
+  // Descomentar o trecho de código abaixo ao implementar a chamada fetch real
+   {/* const res = await fetch("api_de_servicos", {
+        next:
+        {
+          revalidate: 'tempo para revalidar os dados'
+        }
+      })
+    */}
+
+    const data = await res.json();
+    return Response.json(data)
+
+  } catch (error) {
+    throw error
+  }
 }
