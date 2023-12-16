a4ca8c44f9d1c38016b80df4e79dfb4d139083b5
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-12-16 19:11:35 -0300

    fix: corrigido altura do card para dispositivos moveis

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 
 export default function Card({ data }: CardProps) {
   return (
-    <div className=" w-full h-[505px] rounded-lg shadow-gray-800 shadow-inner  relative overflow-hidden transform hover:scale-105 duration-500 ">
+    <div className=" w-full h-[505px] rounded-lg shadow-gray-800 shadow-inner  relative overflow-hidden transform hover:scale-105 duration-500 max-md:h-[405px] ">
       <Image src={data.image} alt="" objectFit="cover" layout="fill" />
       <div className="absolute w-full p-4 bottom-0 ">
         <div className="flex flex-col gap-3 p-4 bg-gray-200 rounded-lg bg-opacity-25 transition-all hover:bg-opacity-50">
