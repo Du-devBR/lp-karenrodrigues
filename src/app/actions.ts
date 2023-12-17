1011633b4a1d69fe8d6e3232d991866ee084ef01
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-12-17 07:35:57 -0300

    feat: criado action para buscar depoimentos

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,7 +1,15 @@
 
-export async function getData() {
+export async function getDataServicos() {
   const revalidarDadosACada24Horas = 5
   const res = await fetch("http://localhost:3000/api/servicos", {next: {revalidate: revalidarDadosACada24Horas}});
+
+  const data = await res.json()
+  return data;
+}
+
+export async function getDataDepoimentos() {
+  const revalidarDadosACada24Horas = 5
+  const res = await fetch("http://localhost:3000/api/depoimentos", {next: {revalidate: revalidarDadosACada24Horas}});
 
   const data = await res.json()
   return data;
