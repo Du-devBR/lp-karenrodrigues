788fcba68c9014564c8ea15122513efd8c92704e
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-12-17 08:04:32 -0300

    feat: adicionado modulo publicRuntimeConfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
+
+
+module.exports = {
+  publicRuntimeConfig: {
+    API_URL: process.env.API_URL,
+  },
+};
+
 module.exports = nextConfig
