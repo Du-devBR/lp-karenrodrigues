f0fd5796b5f6128bef2873208a2c264579f9eb29
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-12-17 08:05:40 -0300

    feat: adicionado .env ao actions

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,7 +1,11 @@
+import getConfig from "next/config";
+
+const {publicRuntimeConfig } = getConfig()
+const API_URL = publicRuntimeConfig.API_URL
 
 export async function getDataServicos() {
   const revalidarDadosACada24Horas = 5
-  const res = await fetch("http://localhost:3000/api/servicos", {next: {revalidate: revalidarDadosACada24Horas}});
+  const res = await fetch(`${API_URL}/servicos`, {next: {revalidate: revalidarDadosACada24Horas}});
 
   const data = await res.json()
   return data;
@@ -9,7 +13,7 @@ export async function getDataServicos() {
 
 export async function getDataDepoimentos() {
   const revalidarDadosACada24Horas = 5
-  const res = await fetch("http://localhost:3000/api/depoimentos", {next: {revalidate: revalidarDadosACada24Horas}});
+  const res = await fetch(`${API_URL}/depoimentos`, {next: {revalidate: revalidarDadosACada24Horas}});
 
   const data = await res.json()
   return data;
