1ec6828f1b4c3747c875ff6d5391fdd3b1f190c0
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-12-19 15:26:35 -0300

    feat: criado arquvio .ts para dados do menu-nav

---
+++ src/assets/menu-data.ts
@@ -0,0 +1,31 @@
+export interface MenuItem {
+  title: string;
+  href: string;
+}
+
+export const arrayMenu: MenuItem[] = [
+  {
+    title: "home",
+    href: "/#inicio",
+  },
+  {
+    title: "sobre",
+    href: "/#sobre",
+  },
+  {
+    title: "galeria",
+    href: "/galeria",
+  },
+  {
+    title: "sobrancelhas",
+    href: "/#servicos",
+  },
+  {
+    title: "espaço",
+    href: "/#espaco",
+  },
+  {
+    title: "depoimentos",
+    href: "/#depoimentos",
+  },
+];
