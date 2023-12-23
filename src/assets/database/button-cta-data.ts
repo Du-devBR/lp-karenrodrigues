167fee1294476afa69b37775c9fb670b7968dbe2
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-12-23 19:16:09 -0300

    feat: adicionado shedule

--- src/assets/database/button-cta-data.ts
+++ src/assets/database/button-cta-data.ts
@@ -4,13 +4,18 @@ export const listButtonCta = {
     name: "contato",
     href: "/#contact"
   },
+  schedule: {
+    name: "agendar",
+    href: `https://wa.me/${process.env.PHONE_NUMBER || ""}`,
+  },
   services: {
-    name: "conheça nossas especialidades",
+    name: "conheça mais",
     href: "/#services"
   },
   location: {
     name: "ver no mapa",
     href: "https://www.google.com/maps?q=-23.54728186418212,-46.21310875734265",
     target: "_blank"
-  }
+  },
+
 }
